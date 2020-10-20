3bd6616709a8cef660e29171c0536eb6d249e196
zerofancy <ntutn.top@gmail.com>
2020-10-21 02:01:47 +0800

    md config

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -8,12 +8,8 @@ private val logger by lazy { LoggerFactory.getLogger(Unit::class.java) }
 fun main() {
     val parser = MdConfigParser(File("./input/节流和防抖动.md"))
     parser.apply {
-        logger.debug("$attributes")
-        logger.debug(title)
         logger.debug(author)
-        logger.debug("$tags")
-        logger.debug(summary)
-        logger.debug(publishDate.toString())
-        logger.debug(editDate.toString())
+        author = "XYX${(0..Int.MAX_VALUE).random()}"
+        saveBack()
     }
 }
